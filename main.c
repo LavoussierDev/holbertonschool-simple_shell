c61dc2d706c58730ce2374a6115f57a849b79c0d
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-12-21 10:58:52 -0400

    made syntax changes and cleaned up readability

--- main.c
+++ main.c
@@ -14,34 +14,45 @@ int main(void)
 	{
 		prompt(STDIN_FILENO, buf);
 		line = _getline(stdin);
+
 		if (_strcmp(line, "\n", 1) == 0)
 		{
 			free(line);
 			continue;
 		}
+
 		tokens = tokenizer(line);
 		if (tokens[0] == NULL)
 			continue;
+
 		builtin_status = builtin_execute(tokens);
+
 		if (builtin_status == 0 || builtin_status == -1)
 		{
 			free(tokens);
 			free(line);
 		}
+
 		if (builtin_status == 0)
 			continue;
+
 		if (builtin_status == -1)
 			_exit(EXIT_SUCCESS);
-		flag = 0; /* 0 if full_path is not malloc'd */
+
+		flag = 0;
 		path = _getenv("PATH");
 		fullpath = _which(tokens[0], fullpath, path);
+
 		if (fullpath == NULL)
 			fullpath = tokens[0];
 		else
-			flag = 1; /* if fullpath was malloc'd, flag to free */
+			flag = 1;
+
 		child_status = child(fullpath, tokens);
+
 		if (child_status == -1)
 			errors(2);
+
 		free_all(tokens, path, line, fullpath, flag);
 	}
 	return (0);
