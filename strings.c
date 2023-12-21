87f5b6a7d52fc135909388f83704105cebaf6f47
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-12-21 07:10:06 -0400

    cleaned up string functions and removed unusued strings

--- strings.c
+++ strings.c
@@ -1,78 +1,80 @@
-#include "shell.h"
-#include <stddef.h>
-#include <stdlib.h>
+#include "holberton.h"
 
-char *_strdup(const char *str)
+/**
+ * _strcmp - compares two strings to find out if they are exactly the same
+ * @name: name supplied by user to search for
+ * @variable: variable to compare against
+ * @length: length of name
+ * Return: 1 if strings are equal, -1 if they are not
+ */
+int _strcmp(char *name, char *variable, unsigned int length)
 {
-	char *ptr;
-	int i, len = 0;
+	unsigned int var_length;
+	unsigned int i;
 
-	if (str == NULL)
-		return (NULL);
+	var_length = _strlen(variable);
+	if (var_length != length)
+		return (-1);
 
-	while (str[len])
+	i = 0;
+	while (name[i] != '\0' && variable[i] != '\0')
 	{
-		len++;
+		if (name[i] != variable[i])
+			return (1);
+		i++;
 	}
-
-	ptr = malloc(sizeof(char) * (len + 1));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i <= len; i++)
-		ptr[i] = str[i];
-	return (ptr);
-}
-int _strcmp(char *s1, char *s2)
-{
-	int cmp;
-
-	cmp = (int)*s1 - (int)*s2;
-	while (*s1)
-	{
-		if (*s1 != *s2)
-		break;
-
-		s1++;
-		s2++;
-		cmp = (int)*s1 - (int)*s2;
-	}
-	return (cmp);
+	return (0);
 }
 
-char *_strcat(char *dest, char *src)
+/**
+ * _strncmp - compares two strings
+ * up to given length are the same
+ * @name: name supplied by user to search for
+ * @variable: variable to compare against
+ * @length: length to compare up to
+ * Return: 1 if strings are equal, -1 if they are not
+ */
+int _strncmp(char *name, char *variable, unsigned int length)
 {
-	char *p = dest;
+	unsigned int i;
 
-	while (*p)
-		p++;
-
-	while (*src)
+	i = 0;
+	while (i < length)
 	{
-		*p = *src;
-		p++;
-		src++;
+		if (name[i] != variable[i])
+			return (-1);
+		i++;
 	}
-	*p = '\0';
+	return (1);
+}
+
+/**
+ * *_strcpy - copies string pointed to by src to the buffer pointed to dest
+ * @dest: string destination
+ * @src: string source
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j = _strlen(src);
+
+	for (i = 0; i <= j; i++)
+		dest[i] = src[i];
+
 	return (dest);
 }
-
-char *_strcpy(char *dest, char *src)
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while (src[i])
-	{
-		dest[i] = src[i];
+	while (s[i] != '\0')
 		i++;
-	}
-	return dest;
-}
 
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len])
-		len++;
-	return (len);
+	return (i);
 }
