12491fcbe2aa5eaf9f1ec384a3b681e64d47069f
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-12-21 07:31:38 -0400

    getting valgrind errors from getenv

--- getenv.c
+++ getenv.c
@@ -6,16 +6,16 @@
  */
 char *_getenv(const char *name)
 {
-		char **environ_copy;
-		char *variable, *value, *path;
-		int compare;
-		unsigned int path_length, environ_length, length, i;
+	char **environ_copy;
+	char *variable, *value, *path;
+	int compare;
+	unsigned int path_length, environ_length, length, i;
 
-		environ_length = 0;
-		while (environ[environ_length] != NULL)
-			environ_length++;
-		environ_copy = NULL;
-		environ_copy = copy_env(environ_copy, environ_length);
+	environ_length = 0;
+	while (environ[environ_length] != NULL)
+		environ_length++;
+	environ_copy = NULL;
+	environ_copy = copy_env(environ_copy, environ_length);
 
 	length = _strlen((char *)name);
 	i = 0;
@@ -27,7 +27,7 @@ char *_getenv(const char *name)
 		{
 			value = strtok(variable, "=");
 			value = strtok(NULL, "\n ");
-			if (value == '\0')
+			if (value == NULL)
 			{
 				errors(4);
 				exit(EXIT_FAILURE);
@@ -46,4 +46,42 @@ char *_getenv(const char *name)
 		i++;
 	}
 	return (NULL);
+}
+
+/**
+ * copy_env - copies environment variable
+ * @environ_copy: pointer to copy of environment variable
+ * @environ_length: length of environment variable
+ * Return: double pointer to copy of environment variable
+ */
+char **copy_env(char **environ_copy, unsigned int environ_length)
+{
+	char *variable;
+	unsigned int variable_length;
+	unsigned int i;
+
+	environ_copy = malloc(sizeof(char **) * (environ_length));
+	if (environ_copy == NULL)
+	{
+		errors(3);
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < environ_length)
+	{
+		variable = environ[i];
+		variable_length = _strlen(variable);
+
+		environ_copy[i] = malloc(sizeof(char) * variable_length + 1);
+		if (environ_copy[i] == NULL)
+		{
+			errors(3);
+			return (NULL);
+		}
+		_strcpy(environ_copy[i], environ[i]);
+		i++;
+	}
+
+	return (environ_copy);
 }
